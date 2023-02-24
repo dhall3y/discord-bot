0dfce7a2a6428700e174c4cb5659e0c8711c31c9
d4v1dh <dhalley@null.net>
2023-02-24 20:14:28 +0100

    github profile checker done

--- commands/github.js
+++ commands/github.js
@@ -11,22 +11,27 @@ module.exports = {
 				.setRequired(true)
 				.setAutocomplete(true)),
 	async autocomplete(interaction) {
-		const focusedValue = interaction.options.getFocused();
-		const query = interaction.options.getString('username');
+		const query = interaction.options.getFocused();
 
-		const dictResult = await request(`https://api.github.com/search/users?q=${query}&per_page=3`, {
+		if (!query) {
+			interaction.respond();
+			return;
+		}
+		const dictResult = await request(`https://api.github.com/search/users?q=${query}&per_page=100`, {
 			headers: { 'User-Agent': 'request' },
 		});
 
 		const list = await dictResult.body.json();
 		try {
-			const choices = (list.items).map(x => x.login);
-			const filtered = choices.filter(choice => choice.startsWith(focusedValue));
+			// take json and order logins from user query
+			const filtered = (list.items).filter(choice => choice.login.startsWith(query));
+			// send first 3 user login
 			await interaction.respond(
-				filtered.map(choice => ({ name: choice, value: choice })),
+				filtered.slice(0, 3).map(choice => ({ name: choice.login, value: choice.login })),
 			);
-		} catch {
-			return;
+		}
+		catch (error) {
+			console.error(error);
 		}
 
 	},
@@ -36,8 +41,27 @@ module.exports = {
 			headers: { 'User-Agent': 'request' },
 		});
 
+		const eventRes = await request(`https://api.github.com/users/${query}/events?per_page=1`, {
+			headers: { 'User-Agent': 'request' },
+		});
+
+		const lastEvent = await eventRes.body.json();
 		const user = await dictResult.body.json();
 
-		await interaction.reply(`username: ${user.login}\navatar: ${user.avatar_url}`);
+		const embedResponse = new EmbedBuilder()
+			.setAuthor({ name: '🟢 last activity: +90 days', url: user.html_url })
+			.setThumbnail(user.avatar_url)
+			.setTitle(user.name)
+			.setDescription(user.bio);
+
+		if (typeof lastEvent[0] !== 'undefined') {
+			const lastEventLocalTime = new Date(lastEvent[0].created_at);
+			embedResponse.setAuthor({ name: `🟢  last activity: ${lastEventLocalTime.toDateString().split(' ').slice(1).join(' ')}`, url: user.html_url });
+		}
+		if (user.name == null) {
+			embedResponse.setTitle(user.login);
+		}
+
+		await interaction.reply({ embeds: [embedResponse] });
 	},
 };
